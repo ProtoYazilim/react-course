271480a6831f308345f7310cef673d9af37bb06c
Ali Erenkara <alierenkara@gmail.com>
2022-01-21 23:46:50 +0300

    Lesson 1 -1.2 Init create-react-app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./app.js"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
